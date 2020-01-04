d9c8874d8efccae0210b8a819c5c1bab93affd76
Sherief Farid <sherief@farid.cloud>
2020-01-04 07:32:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,16 +1,9 @@
-# My Code here....
-
-#
-
 def map_to_negativize(source_array)
     source_array.map {|i| i*-1 }
 end
-
-
 def map_to_no_change(source_array)
   source_array.map {|i| i }
 end
-
 def map_to_double(source_array)
   source_array.map {|i| i *2}
 end
@@ -21,6 +14,8 @@ end
 #
 #
 #
-# reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point)
+  source_array.reduce(:+)
+end
 # reduce_to_all_true(source_array)
 # reduce_to_any_true(source_array)
