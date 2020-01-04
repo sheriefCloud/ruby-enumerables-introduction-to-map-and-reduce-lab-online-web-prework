beb67b70ddaec8a24562c7115d5949f7d677151a
Sherief Farid <sherief@farid.cloud>
2020-01-04 07:37:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,7 +14,7 @@ end
 #
 #
 #
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
   source_array.reduce(starting_point) {|sum, num| sum + num}
 end
 # reduce_to_all_true(source_array)
